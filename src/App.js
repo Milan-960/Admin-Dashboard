8d64239d902f7cf232eae95e42f88c4570260b9c
Milan960 <milan.sachani.ms@gmail.com>
2022-05-26 15:02:40 +0200

    styling has been added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./assets/libs/boxicons-2.1.1/css/boxicons.min.css";
+import "./scss/App.scss";
 import Blank from "./pages/Blank";
 import Dashboard from "./pages/Dashboard";
 import MainLayout from "./layout/MainLayout";
